3ea52170cc7cd041728b6bb3e5cee83a984bd7f2
user.name like "Sumeet Dipanee <user. email like "sumitdeepani00@gmail.com>
2024-08-08 14:26:47 +0530

    marker added

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 const http = require("http");
-
 const socketio = require("socket.io");
+
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+io.on("connection", function (socket) {
+  console.log("New user connected:", socket.id);
+
+  socket.on("send-location", function (data) {
+    io.emit("received-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
+});
+
 app.get("/", function (req, res) {
-  res.send("hey");
+  res.render("index");
 });
 
 server.listen(process.env.PORT || 3000);
